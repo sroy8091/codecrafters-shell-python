4c84554dfb84e53b50df3f05c655c441d5e0dd35
Sumit Roy <sroy8091@gmail.com>
2025-05-25 08:56:52 +0530

    REPL stage

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
+        # Wait for user input
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
