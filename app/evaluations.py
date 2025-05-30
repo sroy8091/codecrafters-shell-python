f43b91e838855a8fdc8cb4b3e2013d9f5187ffb5
Sumit Roy <sroy8091@gmail.com>
2025-05-30 07:50:37 +0530

    add PATH executable

--- app/evaluations.py
+++ app/evaluations.py
@@ -1,4 +1,5 @@
-import sys
+import shutil
+import subprocess
 
 from app.mapping import command_evaluations
 
@@ -8,6 +9,8 @@ def evaluate(command):
     # print(premise in command_evaluations)
     if premise in command_evaluations:
         return command_evaluations[premise](*args)
+    elif shutil.which(premise):
+        return subprocess.run([premise] + args, capture_output=False)
     else:
         print(f"{premise}: command not found")
         return None
