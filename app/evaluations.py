2e04915033434f078396c6f6e7f15cbac8428967
Sumit Roy <sroy8091@gmail.com>
2025-05-30 07:07:49 +0530

    type builtin

--- app/evaluations.py
+++ app/evaluations.py
@@ -6,11 +6,11 @@ from app.mapping import command_evaluations
 def evaluate(command):
     premise, *args = command.split()
     # print(premise in command_evaluations)
-    if premise in command_evaluations:
-        return command_evaluations[premise](*args)
-    elif premise == "type":
+    if premise == "type":
         print(handle_type(premise, *args))
         return None
+    elif premise in command_evaluations:
+        return command_evaluations[premise](*args)
     else:
         print(f"{premise}: command not found")
         return None
@@ -20,4 +20,4 @@ def handle_type(premise, *args):
     if args[0] in command_evaluations:
         return f"{args[0]} is a shell builtin"
     else:
-        return f"{premise}: command not found"
+        return f"{args[0]}: not found"
